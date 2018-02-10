478ffd8e4a9e177a90a5aca3bbd5b61583877cba
Jacob Albright <jacob.albright23@gmail.com>
2018-02-10 10:36:16 -0600

    Added 'clear-btn' ( C ) functionality. Created calculation-output div with ul for displaying results on the DOM. Now need to figure out server-side logic to make  calculation and store a historical log for display on the DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    const currentCalculationQueue = []; // store a sequence of values for buttons pressed
+    let currentCalculationQueue = []; // store a sequence of values for buttons pressed
     $('.number-btn').on('click', function () {
         currentCalculationQueue.push($(this).attr('id'));
         $('#screen-interface').append($(this).attr('id'));
@@ -12,6 +12,10 @@ $(document).ready(function(){
         sendData(currentCalculationQueue); // send the equence of values for buttons pressed server-side for calculation
         $('#screen-interface').empty();
     }); // END equals-btn onclick
+    $('.clear-btn').on('click', function(){
+        currentCalculationQueue = []; // empty the currentCalculationQueue;
+        $('#screen-interface').empty(); // clear the screen interface
+    }); // END clear-btn onclick
 
 }); // END document.ready
 
@@ -54,71 +58,3 @@ function getCalculations(){
 
 
 
-
-/*
-
-$(document).ready(function () {
-    sendCalculationRequirements()
-
-   
-    getAllNumbers();
-    $('#number-in-btn').on('click', function(){
-        submitNumber()
-    }); // END #number-in-btn onclick
-   
-
-}); // END document.ready
-
-
-// get numbers and type of operation to be performed on them from DOM
-function sendCalculationRequirements() {
-    let toBeCalculated = {
-        calculationOrder: [],
-    }
-
-    $('.number-btn').on('click', function () {
-        toBeCalculated.calculationOrder.push($(this).attr('id'));
-    });
-
-    $('.op-btn').on('click', function () {
-        toBeCalculated.calculationOrder.push($(this).attr('id'));
-    });
-
-    $('.equals-btn').on('click', function () {
-        // POST...but for now just log something
-        $.ajax({
-            type: 'POST',
-            url: '/calculator/add-to',
-            data: toBeCalculated
-        }).done(function (response) {
-            // run function to get finished calculation back
-            // this function should display the most recent calculation on the screen-interface
-            getCalculations();
-            // then...clear toBeCalculated.calculationOrder
-            toBeCalculated.calculationOrder = [];
-        }).fail(function (error) {
-            console.log(error);
-        }); // END ajax POST
-
-
-    }); // END equals-btn onclick
-
-} // END sendCalculationRequirements
-
-
-function getCalculations() {
-    $.ajax({
-        type: 'GET',
-        url: '/calculator'
-    }).done(function (response) {
-        console.log('Request was successful!');
-        console.log(response);
-        // call some function that puts the response on the DOM
-
-    }).fail(function (error) {
-        console.log(error);
-    }); // END ajax GET
-
-}; // END getCalculations
-
-*/
